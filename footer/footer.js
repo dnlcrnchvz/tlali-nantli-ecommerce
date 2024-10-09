ba3dcbce09b88946c72f3dc1a5008251301b7b5b
Daniela Ceron <dceronchavez09@gmail.com>
2024-10-09 10:08:29 -0600

    feat: agregación de la carpeta footer

---
+++ footer/footer.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let footer = `
+    <footer class="footer">
+        <div class="footer-section">
+            <h2>Administrador</h2>
+        </div>
+        <div class="footer-section">
+            <h2>Información</h2>
+            <ul>
+                <li>Cambios y Devoluciones</li>
+                <li>Métodos de Pago</li>
+                <li>Envío</li>
+                <li>Ayuda</li>
+            </ul>
+        </div>
+        <div class="footer-section">
+            <h2>Redes Sociales</h2>
+            <div class="social-icons">
+                <a href="#" aria-label="Síguenos en Instagram">
+                    <img src="../footer/imagenes/iconoInstagram.png" alt="Instagram">
+                </a>
+                <a href="#" aria-label="Síguenos en Twitter">
+                    <img src="../footer/imagenes/iconoTwitter.png" alt="Twitter">
+                </a>
+                <a href="#" aria-label="Síguenos en Facebook">
+                    <img src="../footer/imagenes/iconoFacebook.png" alt="Facebook">
+                </a>
+            </div>
+        </div>
+    </footer>
+    `;
+    document.getElementById('footer-container').innerHTML = footer; 
+});
