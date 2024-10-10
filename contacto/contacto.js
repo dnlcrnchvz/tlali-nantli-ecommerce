48e6f27c0efb3f6a86aebbb44519685520b8e66a
Circe_Dellop <circe_dellop@outlook.com>
2024-10-09 18:48:14 -0600

    feat: Se reestructura la jerarquia de carpetas y archivos y corrige la ruta de la imagen del logo de linkedin en el archivo contacto.html

--- contacto/contacto.js
+++ contacto/contacto.js
@@ -1,26 +1,6 @@
 //Obtener referencia del formulario
 const mensaje=document.forms["formulario"];
 
-const validador=(nuevoMensaje)=>
-{
-    let valido=true;
-    const nombre = document.querySelector("#nombre");
-    //const email=document.querySelector("#email");
-    
-    if(nuevoMensaje.tel.length!=10)
-    {
-        alert("El número teléfonico no es valido");
-        valido=false;
-    }
-    else if(!(nombre.setAttribute("pattern", "[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+")))
-    {
-        alert("El número teléfonico no es valido");
-        valido=false;
-    }
-
-    return valido;
-}
-
 const siguienteMensaje=async (nuevoMensaje)=>
 {
     const url="https://formsubmit.co/tlalinantliecom@gmail.com";
