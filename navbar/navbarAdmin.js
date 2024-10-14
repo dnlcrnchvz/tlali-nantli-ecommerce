e1c6d2efe726cc4bd3724e3f1cc0ce4446ae3b08
Daniela Ceron <dceronchavez09@gmail.com>
2024-10-14 16:01:57 -0600

    fix: estilo de botones del navbarAdmin.js

--- navbar/navbarAdmin.js
+++ navbar/navbarAdmin.js
@@ -12,19 +12,14 @@ document.addEventListener("DOMContentLoaded", function() {
                     <i class="fas fa-search"></i>
                 </button>
             </form>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+            <button id="crearBtn" class="btn adminButton" type="button">
+                Crear artículo
+                <i class="fas fa-plus"></i>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="/index.html">Crear Artículo</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../catalogo/catalogo.html">Cerrar Sesión</a>
-                    </li>
-                </ul>
-            </div>
+            <button id="salirBtn" class="btn adminButton" type="button">
+                Cerrar sesión
+                <i class="fas fa-sign-out-alt"></i>
+            </button>
         </div>
     </nav>
     `;
