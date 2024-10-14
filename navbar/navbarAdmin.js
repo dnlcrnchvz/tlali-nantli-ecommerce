eb0dd4cc3db5361e96f3f3a94319a6305656be39
Daniela Ceron <dceronchavez09@gmail.com>
2024-10-14 14:16:41 -0600

    feat: agregación del navbar para el CRUD

---
+++ navbar/navbarAdmin.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let navbar = `
+    <nav class="navbar navbar-expand-lg fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/index.html">
+                <img src="../navbar/imagenes/logoSinFondoTlaliNantli.png" alt="Logo" width="30" height="30" class="logo-navbar d-inline-block align-text-top">
+                <h1 class="navbarTitle">Artículos</h1>
+            </a>
+            <form class="d-flex ms-auto" role="search">
+                <input class="formControl me-2 text-center" type="search" placeholder="Barra de Búsqueda" aria-label="Search">
+                <button type="submit" class="searchIcon">
+                    <i class="fas fa-search"></i>
+                </button>
+            </form>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="/index.html">Crear Artículo</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../catalogo/catalogo.html">Cerrar Sesión</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    `;
+    document.getElementById('navbarContainer').innerHTML = navbar; 
+});
