47b409a59fc90f6d0dda6978a8c94d7675d01913
Lorena Raygoza <lorenaraygoza0904@gmail.com>
2024-10-14 16:42:33 -0700

    fix: dandole formato al navbar

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -1,15 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
     let navbar = `
     <nav class="navbar navbar-expand-lg fixed-top">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="../paginaPrincipal/index.html">
-            <img src="../navbar/imagenes/logo-sin-fondo-tlali-nantli.png" alt="Logo" width="30" height="30" class="logo-navbar d-inline-block align-text-top">
-            <h1 class="navbarTitle">Tlali Nantli</h1>
-        </a>
-        <form class="d-flex ms-auto" role="search">
-            <input class="formControl me-2 text-center" type="search" placeholder="  Buscar" aria-label="Search">
-            <button type="submit" class="searchIcon">
-                <i id="botonBusqueda" class="fas fa-search"></i>
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/index.html">
+                <img src="../navbar/imagenes/logoSinFondoTlaliNantli.png" alt="Logo" width="30" height="30" class="logo-navbar d-inline-block align-text-top">
+                <h1 class="navbarTitle">Tlali Nantli</h1>
+            </a>
+            <form class="d-flex ms-auto" role="search">
+                <input class="formControl me-2 text-center" type="search" placeholder="Barra de Búsqueda" aria-label="Search">
+                <button type="submit" class="searchIcon">
+                    <i class="fas fa-search"></i>
+                </button>
+            </form>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
