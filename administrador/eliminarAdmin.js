7f3d32fcf9e5a7b65da5d2d6ed850d0da4c15c68
Circe_Dellop <circe_dellop@outlook.com>
2024-10-16 21:29:12 -0600

    feat: se agrega funcion para borrar productos uno por uno

--- administrador/eliminarAdmin.js
+++ administrador/eliminarAdmin.js
@@ -1,4 +1,26 @@
-const borrarProducto=()=>
+/* const borrarProducto=()=>
 {
     document.querySelector(".card").innerHTML=``;
-};
+}; */
+
+/* const borrarProducto=()=>
+{
+    if(document.getElementById("btn0"))
+    {
+        document.getElementById("card0").innerHTML=``;
+    }
+    else if(document.getElementById("btn1"))
+        {
+            document.getElementById("card1").innerHTML=``;
+        }
+    
+} */
+
+        const borrarProducto = (index) => {
+            const button = document.getElementById(`btn${index}`);
+            const card = document.getElementById(`card${index}`);
+        
+            if (button && card) {
+                card.innerHTML = '';
+            }
+        };
