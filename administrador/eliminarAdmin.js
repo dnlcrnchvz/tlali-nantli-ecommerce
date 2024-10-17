13bc1abde903c51c669663cf2d7805aeea064c7f
Circe_Dellop <circe_dellop@outlook.com>
2024-10-16 23:12:44 -0600

    docs: se agregan comentarios

--- administrador/eliminarAdmin.js
+++ administrador/eliminarAdmin.js
@@ -1,26 +1,8 @@
-/* const borrarProducto=()=>
-{
-    document.querySelector(".card").innerHTML=``;
-}; */
-
-/* const borrarProducto=()=>
-{
-    if(document.getElementById("btn0"))
-    {
-        document.getElementById("card0").innerHTML=``;
-    }
-    else if(document.getElementById("btn1"))
-        {
-            document.getElementById("card1").innerHTML=``;
-        }
-    
-} */
-
-        const borrarProducto = (index) => {
-            const button = document.getElementById(`btn${index}`);
-            const card = document.getElementById(`card${index}`);
+const borrarProducto = (index) => {
+    const button = document.getElementById(`btn${index}`);//Se obtiene el id del botón eliminar
+    const card = document.getElementById(`card${index}`);//Se obtiene el id de la card
         
-            if (button && card) {
-                card.innerHTML = '';
-            }
-        };
+    if (button && card) {//Se verifica que el id del botón y card coincidan para eliminar la card correcta
+        card.innerHTML = '';
+    }
+};
