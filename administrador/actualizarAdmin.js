983f92cad0d7db70dc80cbd7aae2598f63b1f2ed
Alma Valeria <almamartinez19682@gmail.com>
2024-10-18 17:39:10 -0600

    feat: aguegue divs en agregarAdmin.js y un contenedor de alerta

--- administrador/actualizarAdmin.js
+++ administrador/actualizarAdmin.js
@@ -3,26 +3,27 @@ document.addEventListener("DOMContentLoaded", function() {
     <form id="updateForm" class="crudForm">
         <button class="crudFormClose"><i class="fas fa-times"></i></button>
         <h1 class="crudFormTitle">Actualizar producto</h1>
+              
         <div class="mb-3">
-        <label for="updateImagen" class="crudFormLabel">Imagen del producto:</label>
-        <input type="file" id="updateImagen" name="updateImagen" class="crudFormInputFile" accept="image/*">
+            <label for="updateImagen" class="crudFormLabel">Imagen del producto:</label>
+            <input type="file" id="updateImagen" name="updateImagen" class="crudFormInputFile" accept="image/*">
         </div>
         <div class="mb-3">
-        <label for="updateNombre" class="crudFormLabel">Nombre del producto:</label>
-        <input type="text" id="updateNombre" name="updateNombre" class="crudFormInput">
+            <label for="updateNombre" class="crudFormLabel">Nombre del producto:</label>
+            <input type="text" id="updateNombre" name="updateNombre" class="crudFormInput">
         </div>
         <div class="mb-3">
-        <label for="updateDescripcion" class="crudFormLabel">Descripción del producto:</label>
-        <input type="text" id="updateDescripcion" name="updateDescripcion" class="crudFormInput">
+            <label for="updateDescripcion" class="crudFormLabel">Descripción del producto:</label>
+            <input type="text" id="updateDescripcion" name="updateDescripcion" class="crudFormInput">
         </div>
         <div class="mb-3">
-        <label for="updatePrecio" class="crudFormLabel">Precio del producto:</label>
-        <input type="text" id="updatePrecio" name="updatePrecio" class="crudFormInput">
+            <label for="updatePrecio" class="crudFormLabel">Precio del producto:</label>
+            <input type="text" id="updatePrecio" name="updatePrecio" class="crudFormInput">
          </div>
         <button type="submit" class="crudFormButton">Actualizar</button>
     </form>
     `;
-    document.getElementById('actualizarBtn').onlick = function() {
+    document.getElementById('actualizarBtn').onclick = function() {
         document.getElementById('actualizarContainer').style.display = "block";
         document.getElementById('actualizarContainer').innerHTML = actualizar;
         document.querySelector('.crudFormClose').onclick = function() {
