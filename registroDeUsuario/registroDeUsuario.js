dd831442f8a68912e508d256231f0ebf8506d3eb
Lorena Raygoza <lorenaraygoza0904@gmail.com>
2024-10-29 16:20:58 -0700

    feat: añadi archivo de JS para el local storage del registro de usuario

---
+++ registroDeUsuario/registroDeUsuario.js
@@ -0,0 +1,38 @@
+let nombre = [],
+apellidoPaterno = [],
+apellidoMaterno = [],
+telefono = [],
+correo = [],
+contraseña = [],
+confirmacionContraseña = [];
+
+let elementoBotonRegistro = document.querySelector('#botonRegistro');
+
+elementoBotonRegistro.addEventListener('click', registrarUsuario());
+function registrarUsuario(){
+let nombreAdd = document.querySelector('#nombre').value,
+apellidoPaternoAdd = document.querySelector('#apellidoP').value,
+apellidoMaternoAdd = document.querySelector('#apellidoM').value,
+telefonoAdd = document.querySelector('#telefono').value,
+correoAdd = document.querySelector('#correo').value,
+contraseñaAdd = document.querySelector('#contrasenia').value,
+confirmacionContraseñaAdd = document.querySelector('#confircontrasenia').value;
+
+nombre.push(nombreAdd);
+apellidoPaterno.push(apellidoPaternoAdd);
+apellidoMaterno.push(apellidoMaternoAdd);
+telefono.push(telefonoAdd);
+correo.push(correoAdd);
+contraseña.push(contraseñaAdd);
+confirmacionContraseña.push(confirmacionContraseñaAdd);
+
+localStorage.setItem('nombre', JSON.stringify(nombre));
+localStorage.setItem('apellidoPaterno', JSON.stringify(apellidoPaterno));
+localStorage.setItem('apellidoMaterno', JSON.stringify(apellidoMaterno));
+localStorage.setItem('telefono', JSON.stringify(telefono));
+localStorage.setItem('correo', JSON.stringify(correo));
+localStorage.setItem('contraseña', JSON.stringify(contraseña));
+localStorage.setItem('confirmacionContraseña', JSON.stringify(confirmacionContraseña));
+
+}
+console.log(registrarUsuario);
