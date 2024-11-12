edf3819806af4c95caa03752b9dd900fda8e1bda
Circe_Dellop <circe_dellop@outlook.com>
2024-11-11 19:17:48 -0600

    feat: Se agrega una alerta al realizar el registro de usuario exitoso

--- registroDeUsuario/registroDeUsuario.js
+++ registroDeUsuario/registroDeUsuario.js
@@ -10,29 +10,30 @@ let elementoBotonRegistro = document.querySelector('#botonRegistro');
 
 elementoBotonRegistro.addEventListener('click', registrarUsuario);
 function registrarUsuario(){
-let nombreAdd = document.querySelector('#nombre').value,
-apellidoPaternoAdd = document.querySelector('#apellidoP').value,
-apellidoMaternoAdd = document.querySelector('#apellidoM').value,
-telefonoAdd = document.querySelector('#telefono').value,
-correoAdd = document.querySelector('#correo').value,
-contraseñaAdd = document.querySelector('#contrasenia').value,
-confirmacionContraseñaAdd = document.querySelector('#confirContrasenia').value;
+    let nombreAdd = document.querySelector('#nombre').value,
+    apellidoPaternoAdd = document.querySelector('#apellidoP').value,
+    apellidoMaternoAdd = document.querySelector('#apellidoM').value,
+    telefonoAdd = document.querySelector('#telefono').value,
+    correoAdd = document.querySelector('#correo').value,
+    contraseñaAdd = document.querySelector('#contrasenia').value,
+    confirmacionContraseñaAdd = document.querySelector('#confirContrasenia').value;
 
-nombre.push(nombreAdd);
-apellidoPaterno.push(apellidoPaternoAdd);
-apellidoMaterno.push(apellidoMaternoAdd);
-telefono.push(telefonoAdd);
-correo.push(correoAdd);
-contraseña.push(contraseñaAdd);
-confirmacionContraseña.push(confirmacionContraseñaAdd);
+    nombre.push(nombreAdd);
+    apellidoPaterno.push(apellidoPaternoAdd);
+    apellidoMaterno.push(apellidoMaternoAdd);
+    telefono.push(telefonoAdd);
+    correo.push(correoAdd);
+    contraseña.push(contraseñaAdd);
+    confirmacionContraseña.push(confirmacionContraseñaAdd);
 
-localStorage.setItem('nombre', JSON.stringify(nombre));
-localStorage.setItem('apellidoPaterno', JSON.stringify(apellidoPaterno));
-localStorage.setItem('apellidoMaterno', JSON.stringify(apellidoMaterno));
-localStorage.setItem('telefono', JSON.stringify(telefono));
-localStorage.setItem('correo', JSON.stringify(correo));
-localStorage.setItem('contraseña', JSON.stringify(contraseña));
-localStorage.setItem('confirmacionContraseña', JSON.stringify(confirmacionContraseña));
+    localStorage.setItem('nombre', JSON.stringify(nombre));
+    localStorage.setItem('apellidoPaterno', JSON.stringify(apellidoPaterno));
+    localStorage.setItem('apellidoMaterno', JSON.stringify(apellidoMaterno));
+    localStorage.setItem('telefono', JSON.stringify(telefono));
+    localStorage.setItem('correo', JSON.stringify(correo));
+    localStorage.setItem('contraseña', JSON.stringify(contraseña));
+    localStorage.setItem('confirmacionContraseña', JSON.stringify(confirmacionContraseña));
 
+    alert("Su registro se realizo con exito. Inicie sesión con su usuario y contraseña.")
 }
 console.log(registrarUsuario);
